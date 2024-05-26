ccfdb7562be1314529d91fc6385446b566e8ab1f
satya-781 <satyajitdalai781@gmail.com>
2024-05-26 23:28:15 +0530

    THESE ARE THE CODES

---
+++ A.c
@@ -0,0 +1,24 @@
+/*Program to  print capital-B using asterisks*/
+#include <stdio.h>
+int main()
+{
+	int i,j,n,width,height;
+	printf("\nEnter the number of lines=>");
+	scanf("%d",&height);
+	width=(2*height)-1;
+	n=width/2;
+	for (i = 0; i < height; i++) {
+        for (j = 0; j <= width; j++) {
+            if (j == n || j == (width-n) || (i == height / 2 && j > n && j < (width-n))) 
+                printf("*");
+            
+            else 
+                printf(" ");
+            
+        }
+        printf("\n");
+        n--;
+    }
+    return 0;
+	
+}
